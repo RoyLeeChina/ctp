ba6f7818eae8271b4dabb2cce9c61410597974df
王锋 <wangfeng@jiedaibao.com>
2017-03-12 05:50:31 +0800

    add: 组织架构

--- src/main/java/org/hotwheel/stock/exchange/context/pay/PayInnerResult.java
+++
@@ -1,9 +0,0 @@
-package org.hotwheel.stock.exchange.context.pay;
-
-/**
- * Created by wangfeng on 2016/12/22.
- */
-public class PayInnerResult<T> {
-    public PayInnerError error;
-    public T data;
-}
--- src/main/java/org/hotwheel/stock/exchange/service/DebtdataThread.java
+++
@@ -1,7 +0,0 @@
-package org.hotwheel.stock.exchange.service;
-
-/**
- * Created by wangfeng on 2017/1/10.
- */
-public class DebtdataThread {
-}
