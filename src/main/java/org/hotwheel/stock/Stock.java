7d0cf1319acfa95d4ec1581b066a3d5823f54bed
王锋 <wangfengxy@sina.cn>
2017-03-12 08:14:23 +0800

    add: 调整jdk版本到7

---
+++ src/main/java/org/hotwheel/stock/Stock.java
@@ -0,0 +1,43 @@
+package org.hotwheel.stock;
+
+/**
+ * 股票数据常亮
+ *
+ * Created by wangfeng on 2017/3/12.
+ */
+public final class Stock {
+    /**
+     * 五分钟，字符串"5"
+     */
+    public static final String FIVE_MINUTES = "5";
+    /**
+     * 十五分钟，字符串"15"
+     */
+    public static final String FIFTEEN_MINUTES = "15";
+    /**
+     * 三十分钟，字符串"30"
+     */
+    public static final String THIRTY_MINUTES = "30";
+    /**
+     * 一小时，字符串"60"
+     */
+    public static final String ONE_HOUR = "60";
+    /**
+     * 一天，字符串"240"
+     */
+    public static final String ONE_DAY = "240";
+    /**
+     * 一周，字符串"1680"
+     */
+    public static final String ONE_WEEK = "1680";
+    /**
+     * 全部数据，字符串"1000000"
+     */
+    public static final String DEFAULT_DATALEN = "1000000";
+
+    /** 时间格式 */
+    public final static String TimeFormat = "yyyy-MM-dd HH:mm:ss";
+    /** 日期格式 */
+    public final static String DateFormat = "yyyy-MM-dd";
+    public final static String DateFormat2 = "yyyyMMdd";
+}
---
+++ src/test/java/org/hotwheel/stock/test/ToolsTest.java
@@ -0,0 +1,29 @@
+package org.hotwheel.stock.test;
+
+import org.hotwheel.stock.data.Tools;
+import org.junit.Test;
+
+import org.junit.Assert.*;
+
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * 
+ * @author yilihjy Email:yilihjy@gmail.com
+ * @version 1.0.0
+ *
+ */
+public class ToolsTest {
+
+	@Test
+	public void testString2LocalDateTimeMethod() {
+		String time1 = "2017-01-05";
+		String time2 = "2017-01-05 15:00:00";
+		Date ltime = new Date(2017, 1, 5, 15, 0);
+		assertEquals(ltime, Tools.string2LocalDateTime(time1));
+		assertEquals(ltime, Tools.string2LocalDateTime(time2));
+	}
+
+}
