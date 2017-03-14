037f1ef3932e1501b37fbcb94801b684f2fcb7aa
王锋 <wangfeng@jiedaibao.com>
2017-03-14 08:50:41 +0800

    update: 数据表

--- src/main/java/org/hotwheel/stock/exchange/controller/MonitorController.java
+++ src/main/java/org/hotwheel/stock/exchange/controller/MonitorController.java
@@ -1,6 +1,7 @@
 package org.hotwheel.stock.exchange.controller;
 
 import org.hotwheel.io.ActionStatus;
+import org.hotwheel.stock.StockOptions;
 import org.hotwheel.stock.dao.IStockSubscribe;
 import org.hotwheel.stock.model.StockSubscribe;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,7 @@ public class MonitorController {
         StockSubscribe info = stockSubscribe.select(phone, code);
         if (info == null) {
             info = new StockSubscribe();
-            info.setFlag("01");
+            info.setFlag(StockOptions.kNormalState);
             info.setPhone(phone);
             info.setCode(code);
             int result = stockSubscribe.insert(info);
