f20c44e931286fee6a13402ca71a4620b60b92df
王锋 <wangfeng@jiedaibao.com>
2017-03-14 12:12:56 +0800

    update: 数据表

--- src/main/java/org/hotwheel/stock/exchange/controller/MonitorController.java
+++ src/main/java/org/hotwheel/stock/exchange/controller/MonitorController.java
@@ -1,5 +1,6 @@
 package org.hotwheel.stock.exchange.controller;
 
+import org.hotwheel.assembly.Api;
 import org.hotwheel.io.ActionStatus;
 import org.hotwheel.stock.StockOptions;
 import org.hotwheel.stock.dao.IStockSubscribe;
@@ -28,29 +29,56 @@ public class MonitorController {
         ActionStatus resp = new ActionStatus();
         int errno = 10000;
         String message = "订阅已经存在";
-        StockSubscribe info = stockSubscribe.select(phone, code);
-        int result = -1;
-        if (info == null) {
-            info = new StockSubscribe();
-            info.setFlag(StockOptions.kNormalState);
-            info.setPhone(phone);
-            info.setCode(code);
-            result = stockSubscribe.insert(info);
-            if (result == 1) {
-                resp.set(0, "SUCCESS");
-            } else {
-                resp.set(errno + 1, "添加订阅失败");
-            }
+        if (Api.isEmpty(phone)) {
+            // 手机号码为空
+            resp.set(errno + 1, "手机号码不能为空");
+        } else if (Api.isEmpty(code)) {
+            // 代码为空
+            resp.set(errno + 2, "股票代码不能为空");
         } else {
-            info.setCode(code);
-            info.setCreateTime(new Date());
-            result = stockSubscribe.update(info);
-            if (result == 1) {
-                resp.set(0, "SUCCESS");
+            code = code.toLowerCase();
+            if (code.startsWith("sh") || code.startsWith("sz")) {
+                // 代码前缀正确
+                code = code.substring(2);
             } else {
-                resp.set(errno, message);
+                // 非sh或sz开头
             }
 
+            if (!Api.isInteger(code)) {
+                // 非数字
+                resp.set(errno + 3, "无效的代码格式");
+            } else {
+                // 如果是纯数字
+                if (code.startsWith("6")) {
+                    code = "sh" + code;
+                } else {
+                    code = "sz" + code;
+                }
+
+                StockSubscribe info = stockSubscribe.select(phone, code);
+                int result = -1;
+                if (info == null) {
+                    info = new StockSubscribe();
+                    info.setFlag(StockOptions.kNormalState);
+                    info.setPhone(phone);
+                    info.setCode(code);
+                    result = stockSubscribe.insert(info);
+                    if (result == 1) {
+                        resp.set(0, "SUCCESS");
+                    } else {
+                        resp.set(errno + 1, "添加订阅失败");
+                    }
+                } else {
+                    info.setCode(code);
+                    info.setCreateTime(new Date());
+                    result = stockSubscribe.update(info);
+                    if (result == 1) {
+                        resp.set(0, "SUCCESS");
+                    } else {
+                        resp.set(errno, message);
+                    }
+                }
+            }
         }
 
         return resp;
