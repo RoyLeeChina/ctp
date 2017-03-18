c172db4d12584c89eba6758661053edb35328096
王锋 <wangfeng@jiedaibao.com>
2017-03-18 23:00:54 +0800

    增加指标文件

--- src/main/java/org/hotwheel/ctp/model/EMA.java
+++ src/main/java/org/hotwheel/ctp/model/EMA.java
@@ -6,6 +6,31 @@ package org.hotwheel.ctp.model;
  * @version 1.0.1
  */
 public class EMA {
+    private int cycle;
     private double ema1;
     private double ema2;
+
+    public int getCycle() {
+        return cycle;
+    }
+
+    public void setCycle(int cycle) {
+        this.cycle = cycle;
+    }
+
+    public double getEma1() {
+        return ema1;
+    }
+
+    public void setEma1(double ema1) {
+        this.ema1 = ema1;
+    }
+
+    public double getEma2() {
+        return ema2;
+    }
+
+    public void setEma2(double ema2) {
+        this.ema2 = ema2;
+    }
 }
