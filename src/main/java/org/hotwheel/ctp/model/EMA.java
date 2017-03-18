048ca549d1b6fdfd6f40b89ba392d45c4d390f07
王锋 <wangfengxy@sina.cn>
2017-03-18 23:04:58 +0800

    增加指标文件

--- src/main/java/org/hotwheel/ctp/model/EMA.java
+++ src/main/java/org/hotwheel/ctp/model/EMA.java
@@ -2,20 +2,33 @@ package org.hotwheel.ctp.model;
 
 /**
  * EMA指标数据
+ *
  * Created by wangfeng on 2017/3/18.
  * @version 1.0.1
  */
 public class EMA {
-    private int cycle;
+    // 周期1
+    private int cycle1;
+    // 周期2
+    private int cycle2;
+
     private double ema1;
     private double ema2;
 
-    public int getCycle() {
-        return cycle;
+    public int getCycle1() {
+        return cycle1;
     }
 
-    public void setCycle(int cycle) {
-        this.cycle = cycle;
+    public void setCycle1(int cycle1) {
+        this.cycle1 = cycle1;
+    }
+
+    public int getCycle2() {
+        return cycle2;
+    }
+
+    public void setCycle2(int cycle2) {
+        this.cycle2 = cycle2;
     }
 
     public double getEma1() {
