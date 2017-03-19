12000e2424f52a446462a19d4ea47adff90e31f5
王锋 <wangfengxy@sina.cn>
2017-03-19 14:14:32 +0800

    增加指标文件, MACD

---
+++ src/main/java/org/hotwheel/ctp/dao/IStockCode.java
@@ -0,0 +1,9 @@
+package org.hotwheel.ctp.dao;
+
+/**
+ * 股票代码DAO
+ * Created by wangfeng on 2017/3/19.
+ */
+public class IStockCode {
+
+}
---
+++ src/main/java/org/hotwheel/ctp/model/StockCode.java
@@ -0,0 +1,11 @@
+package org.hotwheel.ctp.model;
+
+/**
+ * 股票代码数据
+ *
+ * Created by wangfeng on 2017/3/19.
+ * @version 1.0.1
+ */
+public class StockCode {
+
+}
