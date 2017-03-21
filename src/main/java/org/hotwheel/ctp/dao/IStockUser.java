9a7425ba009078e537414c55adaf5d4795a28bf3
王锋 <wangfengxy@sina.cn>
2017-03-22 04:51:12 +0800

    调整日志格式

--- src/main/java/org/hotwheel/ctp/dao/IStockUser.java
+++ src/main/java/org/hotwheel/ctp/dao/IStockUser.java
@@ -2,7 +2,10 @@ package org.hotwheel.ctp.dao;
 
 import org.apache.ibatis.annotations.Param;
 import org.hotwheel.ctp.model.User;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * 用户操作接口
@@ -13,14 +16,15 @@ import org.springframework.stereotype.Repository;
 @Repository("stockUser")
 public interface IStockUser {
 
-    public User select(@Param("phone") String phone);
+    public List<User> selectAll() throws DataAccessException;
+    public User select(@Param("phone") String phone) throws DataAccessException;
 
     /**
      * 创建一个新用户
      * @param user
      * @return
      */
-    public int insert(User user);
+    public int insert(User user) throws DataAccessException;
 
-    public int update(User user);
+    public int update(User user) throws DataAccessException;
 }
