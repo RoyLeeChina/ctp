1a079dc96d424115d075915ead7be9b668135172
王锋 <wangfeng@jiedaibao.com>
2017-03-23 03:06:45 +0800

    增加策略下行消息历史记录表

--- src/main/java/org/hotwheel/ctp/dao/IStockMessage.java
+++ src/main/java/org/hotwheel/ctp/dao/IStockMessage.java
@@ -1,6 +1,8 @@
 package org.hotwheel.ctp.dao;
 
+import org.apache.ibatis.annotations.Param;
 import org.hotwheel.ctp.model.StockMessage;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -10,8 +12,11 @@ import java.util.List;
  * Created by wangfeng on 2017/3/22.
  * @version 1.0.2
  */
+@Service("stockMessage")
 public interface IStockMessage {
+    public int cleanAll();
     public List<StockMessage> selectAll(final String type);
     public int insert(final StockMessage info);
     public int update(final StockMessage info);
+    public int updateType(@Param("id") long id, @Param("type") String type);
 }
---
+++ src/main/java/org/hotwheel/ctp/exchange/task/PushMessageTask.java
@@ -0,0 +1,66 @@
+package org.hotwheel.ctp.exchange.task;
+
+import org.hotwheel.assembly.Api;
+import org.hotwheel.ctp.StockOptions;
+import org.hotwheel.ctp.dao.IStockMessage;
+import org.hotwheel.ctp.dao.IStockUser;
+import org.hotwheel.ctp.model.StockMessage;
+import org.hotwheel.ctp.model.UserInfo;
+import org.hotwheel.ctp.util.EmailApi;
+import org.hotwheel.spring.scheduler.SchedulerContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 消息推送任务
+ *
+ * Created by wangfeng on 2017/3/22.
+ * @version 1.0.2
+ */
+@Service("pushMessageTask")
+public class PushMessageTask extends SchedulerContext {
+    private static Logger logger = LoggerFactory.getLogger(PushMessageTask.class);
+
+    @Autowired
+    private IStockMessage stockMessage;
+
+    @Autowired
+    private IStockUser stockUser;
+
+    @Override
+    protected void service() {
+        while (true) {
+            if (isTimeExpire()) {
+                logger.info("运行时间{}->{}到, 任务退出", taskStartTime, taskEndTime);
+                break;
+            }
+            stockMessage.cleanAll();
+            // 捡出全部未发送的消息
+            List<StockMessage> messageList = stockMessage.selectAll("00");
+            if (messageList != null && messageList.size() > 0) {
+                for (StockMessage message : messageList) {
+                    long id = message.getId();
+                    String type = "01";
+                    String phone = message.getPhone();
+                    UserInfo userInfo = stockUser.select(phone);
+                    if (userInfo == null) {
+                        type = "97";
+                    } else {
+                        boolean bResult = EmailApi.send(userInfo.getEmail(), "CTP策略提示", message.getRemark());
+                        if (bResult) {
+                            type = "01";
+                        } else {
+                            type = "02";
+                        }
+                    }
+                    stockMessage.updateType(id, type);
+                }
+            }
+            Api.sleep(StockOptions.kRealTimenterval);
+        }
+    }
+}
