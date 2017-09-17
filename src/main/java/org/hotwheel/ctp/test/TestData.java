1788df5a5fec346a1cfef847a99203d04625e6bd
王锋 <wangfengxy@sina.cn>
2017-09-17 23:15:03 +0800

    实时数据监控增加资金流向提示

--- src/main/java/org/hotwheel/ctp/test/TestData.java
+++ src/main/java/org/hotwheel/ctp/test/TestData.java
@@ -7,6 +7,7 @@ import org.hotwheel.core.io.Resource;
 import org.hotwheel.ctp.data.MoneyFlowUtils;
 import org.hotwheel.ctp.model.StockMoneyFlow;
 import org.hotwheel.ctp.util.ExcelApi;
+import org.hotwheel.json.JsonAdapter;
 
 import java.io.IOException;
 
@@ -20,6 +21,7 @@ public class TestData {
     public static void main(String[] args) {
 
         StockMoneyFlow moneyFlow = MoneyFlowUtils.getOne("sz000789");
+        System.out.println(JsonAdapter.get(moneyFlow, false));
         String text = "({r0_in:\"35900722.2600\",r0_out:\"76097226.6100\",r0:\"115435711.8700\",r1_in:\"133697056.4000\",r1_out:\"151195932.8900\",r1:\"292575412.2900\",r2_in:\"67635481.0600\",r2_out:\"74890046.9700\",r2:\"147907606.0300\",r3_in:\"19164795.2600\",r3_out:\"17796232.6200\",r3:\"38082645.8800\",curr_capital:\"30997\",name:\"中船科技\",trade:\"18.8400\",changeratio:\"-0.0253492\",volume:\"31195191.0000\",turnover:\"1006.4\",r0x_ratio:\"-110.535\",netamount:\"-63581384.1100\"})";
         String str = text.substring(1, text.length() - 1);
         Object objJson = JSON.parse(str);
