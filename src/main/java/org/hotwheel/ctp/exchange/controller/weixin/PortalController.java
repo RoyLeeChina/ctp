0fa02658dd52bb9364386c57f1fe205b8e0bb49f
王锋 <wangfengxy@sina.cn>
2018-04-19 22:07:53 +0800

    增加基本判断信息

--- src/main/java/org/hotwheel/ctp/exchange/controller/weixin/PortalController.java
+++ src/main/java/org/hotwheel/ctp/exchange/controller/weixin/PortalController.java
@@ -215,7 +215,7 @@ public class PortalController implements WeChatContext {
                                 content += "，该股今日选择方向";
                             } else {
                                 String gl = String.format("%.2f", 100 * p0);
-                                content += "该股上涨概率" + gl + "%";
+                                content += "，该股上涨概率" + gl + "%";
                             }
                             String prefix = Api.toString(new Date(), "yyyy年MM月dd日");
                             String title = prefix + "-CTP策略提示(" + Api.toString(info.getCreateTime(), StockOptions.TimeFormat) + ")";
